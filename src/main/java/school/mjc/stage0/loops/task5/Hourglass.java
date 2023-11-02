2a64c65ed6dd163f112ebfed4c3d1865e3ba984e
Abdulxafizx9 <abdulxafizx999@gmail.com>
2023-11-02 23:13:32 +0500

    done

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,23 +1,26 @@
+
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
-    /*public static void main(String[] args){
-        int height=6;*/
-        for(int i = 0;i<height;i++)
-        {
-            int noOfCol=i<=2?height-2*i:2+(i-3)*2;
-            int space = noOfCol%3;
+  /*  public static void main(String[] args){
+        int height = 5;*/
+  public void printHourglassOfGivenSize(int height) {
+        for(int i = 0; i < height; i++) {
+            int noOfCol;
+            if(height % 2 == 0)
+                noOfCol = i < height / 2 ? height - 2 * i : 2 * (i - height / 2 + 1);
+            else
+                noOfCol = i <= height / 2 ? height - 2 * i : 2 * (i - height / 2) + 1;
+
+            int space = (height - noOfCol) / 2;
             for (int k = 0; k < space; k++) {
                 System.out.print(" ");
             }
-            for(int j = 0;j<noOfCol;j++){
-
-                   System.out.print("8");
-
+            for(int j = 0; j < noOfCol; j++){
+                System.out.print("8");
             }
             System.out.println();
         }
-
     }
 }
+
